81ba1c53bf618ee47b439d6493a02fcb9eeb3f42
asus test <msa-99@outlook.com>
2024-05-05 14:05:21 +0600

    recursion call first

---
+++ reversePrintByRecursion.c
@@ -0,0 +1,12 @@
+//age recursion function call hobe pore print hobe.
+
+#include<stdio.h>
+void fun(int i){
+    if(i==6) return;
+    fun(i+1);
+    printf("%d ",i);
+}
+int main(){
+    fun(1);
+    return 0;
+}
