7f516bad4627f5b99b81c6f27e82a11424e3806a
asus test <msa-99@outlook.com>
2024-06-02 22:24:07 +0600

    calSumOfArrayElem

--- sumOfAllElemByRecursion.cpp
+++ sumOfAllElemByRecursion.cpp
@@ -1,14 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int calArraySum(int nums[], int size){
-    for(int i=0; i<size; i++){
-        cout<<nums[i];
+    //zodi array empty hoy tahole 0 return kore dite hobe.
+    int total=0;
+    tota+=nums[size];
+    if(size==0){
+        return tota+=nums[size];
     }
-    return 0;
+    calArraySum(nums, size-1);
+    return total;
+    
+
+
 }
+
 int main(){
     int nums[5]={2,4,8,3,2};
     int size=sizeof(nums)/sizeof(nums[0]);
-    int value=calArraySum(nums, size);
+    int value=calArraySum(nums, size-1);
+    cout<<value;
     return 0;
 }
