44874337d9f1f0dafe5f8070b5399021bd1b4658
asus test <msa-99@outlook.com>
2024-06-02 23:22:07 +0600

    cal factorial by recursion [DONE]

--- factorialByRecursion.cpp
+++ factorialByRecursion.cpp
@@ -4,8 +4,12 @@ int calFactorial(int n){
     if(n==0 || n==1){
         return 1;
     }
-    int val=calFactorial(n-1);
-    return val*n;
+    // 1 approach
+    // int val=calFactorial(n-1);
+    // return val*n;
+
+    // 2 short approach
+    return n*calFactorial(n-1);
 }
 int main(){
     int n;
