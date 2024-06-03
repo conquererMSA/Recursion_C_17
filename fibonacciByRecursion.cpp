d2388b8f3714beec0c7b8dbee98195fc92311214
asus test <msa-99@outlook.com>
2024-06-03 21:24:09 +0600

    findFibonacciByRecursion

---
+++ fibonacciByRecursion.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findFibonacci(int n){
+    //Recursion e ekadik baseCase use kora zay
+  //baseCase 1
+  if(n==0){
+    return 0;
+  }
+  //baseCase 2
+   if(n==1){
+    return 1;
+  }
+  //first precding number before n-1
+  int one=findFibonacci(n-1);
+  //last preceding number before n-2
+  int two=findFibonacci(n-2);
+  return one+two;
+  
+}
+int main(){
+    int input;
+    cin>>input;
+    int fiboResult=findFibonacci(input);
+    cout<<"fibonacci for "<<input<<" is: "<<fiboResult;
+    return 0;
+}
