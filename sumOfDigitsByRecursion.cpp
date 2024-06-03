4da73c34b3682b91dfe557a2f6f6a5465c91eaa1
asus test <msa-99@outlook.com>
2024-06-03 21:55:43 +0600

    sumOfDigitByRecursion

---
+++ sumOfDigitsByRecursion.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+//cout<<123%10<<endl;//3
+//   cout<<123/10;//12
+//**return findSumOfDigits(n/10)%10;
+
+// int lastDigit=n%10;
+// return lastDigit+findSumOfDigits(n/10);
+
+int findSumOfDigits(int n){
+   if(n<9){
+    return n;
+   }
+int lastDigit=n%10;
+int due=findSumOfDigits(n/10);
+int oneTimeValue= due+lastDigit;
+return oneTimeValue;
+
+
+}
+int main(){
+    int input;
+    cin>>input;
+    int result=findSumOfDigits(input);
+    cout<<result;
+    return 0;
+}
