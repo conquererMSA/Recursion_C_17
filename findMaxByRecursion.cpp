3339e536630af64052f346fc0bb26a9361b7488d
asus test <msa-99@outlook.com>
2024-06-04 00:15:34 +0600

    findMinByRecursion [DONE]

--- findMaxByRecursion.cpp
+++ findMaxByRecursion.cpp
@@ -6,7 +6,9 @@ int findMax(float arr[], int index) {
         return arr[index];
     }
     float lastEle=findMax(arr, index+1); //only return last elem of array, then we compare every time with index number
+    //ekhane findMin func er index barte barte zokhon last index e cole zabe , skhan teke array er last elem return hobe. last index er elem er sathe er ager ghorer elem compare korbo. ekhane findMin func shudhu matro last element return kore, onno index gulur kkhetre index diye access kora hoy. ekhane findMin function reversly elem return korbe.
     return lastEle>arr[index]?lastEle:arr[index]; //for integer elem
+    //ekhane findMin er return kora array er last elem er sathe findMin function er current index eleme access kore compare kora hoy. zeti smaller seti return kora hocce.
 
     // for float number
     // int max=INT_MIN;
